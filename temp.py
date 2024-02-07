7b343a0636dabc1283451093712dec526bb45ce7
Corentin <corentindavion@gmail.com>
2024-02-07 15:21:47 +0100

    added the modification to add the error bar

--- temp.py
+++ temp.py
@@ -27,7 +27,7 @@ def exp_temp(debut,fin,tries,temp,x=0.01):
     k=np.log(A/(temp-fin))/C
     return A*np.exp(-k*C)
 
-def temperature_heuristic(instance,sol_init,dist_mat,tries=100000,temp_init=500,temp_end=10,update_t=None,voisin=None):
+def temperature_heuristic(instance,sol_init,dist_mat,tries=10000,temp_init=5000,temp_end=10,update_t=None,voisin=None):
     temp=temp_init
     curr_sol=sol_init
     score=compute_score_with_mat(instance,curr_sol,dist_mat)
@@ -63,10 +63,24 @@ def temperature_heuristic(instance,sol_init,dist_mat,tries=100000,temp_init=500,
         if update_t is None:
             temp=temp-(temp_init-temp_end)/tries
         else:
-            temp=update_t(temp_init,temp_end,tries,temp)
+            temp=temp*(temp_end/temp_init)**(1/tries)
         temp_list.append(temp)
     return curr_sol, score_list,temp_list,nb_violation
 
+def average_and_error(mat):
+    avg=[]
+    error=[]
+    for i in range(len(mat[0])):
+        temp=np.zeros(len(mat))
+        for j in range(len(mat)):
+            temp[j]=mat[j][i]
+        average=np.average(temp)
+        avg.append(average)
+        #low=np.percentile(temp,2.5)
+        #up=np.percentile(temp,97.5)
+        #error.append(temp[(temp >= low) & (temp <= up)])
+        error.append(max(temp)-min(temp)*0.5)
+    return avg, error
 def main():
     inst ="data/inst1"
 
@@ -78,25 +92,32 @@ def main():
     keys=instance.keys()
     keys=list(keys)
     #print(type(keys[0]))
+    hold=[]
 
     mat1=array(mat1) #convert to numpy array so that score doens't f up
-    sol_init=first_sol(instance)
+   
     #print(sol_init)
-    print(compute_score_with_mat(instance,sol_init,mat1))
-    min=-1
-    for i in range(1):
-        sol,score_list,temp_list,nb_violation=temperature_heuristic(instance,sol_init,mat1,update_t=None) 
+    #print(compute_score_with_mat(instance,sol_init,mat1))
+    #min=-1
+    for i in range(10):
+        sol_init=first_sol(instance)
+        _,score_list,_,nb_violation=temperature_heuristic(instance,sol_init,mat1,update_t=exp_temp) 
+        hold.append(score_list)
         print(nb_violation)
-        if min<0 or score_list[-1]<min:
-            min=score_list[-1] 
+        #if min<0 or score_list[-1]<min:
+        #    min=score_list[-1] 
+    avg,error=average_and_error(hold)
+    error=np.array(error)
+    error_indices = np.arange(len(avg)) % 1000 == 0
     
-    plt.plot([i for i in range(len(score_list))],score_list)
+    plt.errorbar([i for i in range(len(score_list))],avg,yerr=[error[i] if error_indices[i] else 0 for i in range(len(error))])
     # plt.gca().invert_xaxis(); probleme de proba trop elevel; augmente le nombre d iteration. bien baisser les probas. fonction de score: afficher le score
     # pour une meme heuristiaue;courbe+ Intervalle de confiance: avec plusieur fonction de voisinage; et plusieur jeux de parametre.
     
     plt.ylabel("score")
     plt.xlabel("iteration") 
-    plt.title(f'Score as a function of temp, with {sol} as the solution for {inst}')
+    #plt.xscale('log')
+    plt.title(f'Average function of score as a function of iterations, with 95% errors as the solution for {inst}')
     plt.show()
 
     
